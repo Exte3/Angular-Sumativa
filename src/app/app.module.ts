1004b864173407e77ef041321606825936f3ff8c
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-09-24 12:40:32 -0300

    Se agregaron componentes y una imagen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
