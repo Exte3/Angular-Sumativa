bc3a09452c5e8029209c712dc178076e76b92227
Gabriel <nunkmeconecto@gmail.com>
2021-09-29 00:15:24 -0300

    Se agregan placeholder a la funcion de modificar

--- src/app/general/modificarproducto/modificarproducto.component.ts
+++ src/app/general/modificarproducto/modificarproducto.component.ts
@@ -10,4 +10,14 @@ export class ModificarproductoComponent implements OnInit {
   constructor(public productoService: ProductoService) {}
 
   ngOnInit(): void {}
+  actualizarProducto(Producto: any): void {
+    //placeholder, aquí debería ser más un form con el id que recibe para cambiar
+    //falta cambiar la vista
+    console.log('Se actualiza el producto', Producto.id);
+  }
+
+  eliminarProduct(id: any): void {
+    //tambien es un placeholder
+    console.log('Se ha eliminado producto', id);
+  }
 }
